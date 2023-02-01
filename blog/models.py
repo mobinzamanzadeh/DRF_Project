b40d9c547e75405fbc56c67d6d9b695825006062
Mobin Zamanzadeh <mobin.zamanzadeh@gmail.com>
2023-02-02 02:40:05 +0330

    make models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,22 @@
+from datetime import timezone
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    slug = models.SlugField()
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    publish = models.DateTimeField(default=datetime.now(timezone.utc))
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(False)
+
+    def __str__(self):
+        return self.title
+
+
+
