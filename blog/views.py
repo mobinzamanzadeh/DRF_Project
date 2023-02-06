b5a77611f707ba1c5ecde75a4d3b6b3b9bde355d
Mobin Zamanzadeh <108784865+mobinzamanzadeh@users.noreply.github.com>
2023-02-06 02:36:14 -0800

    Update views.py
    add detail

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,12 @@ from .models import Article
 class ArticleList(ListView):
     def get_queryset(self):
         return Article.objects.filter(status=True)
+    
+    
 
+class ArticDetail(DetailView):
+    def get_object(self):
+        return get_object_or_404(
+            Article,
+            pk=self.kwargs.get("pk"),
+        )
