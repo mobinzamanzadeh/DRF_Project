8dc32cc5177231addbf0a5b3e06a94804df1c4c2
Mobin Zamanzadeh <mobin.zamanzadeh@gmail.com>
2023-02-17 15:53:47 +0330

    router

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,20 @@
 from django.urls import path, include
 from .views import *
+from rest_framework import routers
 
-urlpatterns = [
-    path("", ArticleList.as_view(), name="list"),
-    path("<int:pk>", ArticleDetail.as_view(), name="detail"),
-    path("users/", UserList.as_view(), name="user-list"),
-    path("users/<int:pk>", UserDetail.as_view(), name="user-detail"),
-]
+
+router = routers.SimpleRouter()
+router.register('', ArticleViewSet)
+router.register('users', UserViewSet)
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('', include(router.urls))
+# ]
+
+# urlpatterns = [
+#     # path("", ArticleList.as_view(), name="list"),
+#     # path("<int:pk>", ArticleDetail.as_view(), name="detail"),
+#     # path("users/", UserList.as_view(), name="user-list"),
+#     # path("users/<int:pk>", UserDetail.as_view(), name="user-detail"),
+# ]
